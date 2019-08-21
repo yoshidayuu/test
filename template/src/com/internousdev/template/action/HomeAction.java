3c339372b388c57da1d3e1e117eed12d5a803932
Takahashiyuuri <haru.isk@gmail.com>
2019-08-21 19:51:33 +0900

    0821

---
+++ template/src/com/internousdev/template/action/HomeAction.java
@@ -0,0 +1,7 @@
+package com.internousdev.template.action;
+import com.opensymphony.xwork2.ActionSupport;
+public class HomeAction extends ActionSupport {
+public String execute() {
+return SUCCESS;
+}
+}
